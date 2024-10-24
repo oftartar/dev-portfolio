cd37e62a1bf229bbedb49f6b7e81c4f8f2b24d7f
Ömer Faruk TARTAR <oftartar@gmail.com>
2024-10-24 14:03:40 +0300

    implement footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./sections/About";
 import Clients from "./sections/Clients";
 import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 import Projects from "./sections/Projects";
@@ -15,6 +16,7 @@ const App = () => {
       <Clients />
 
       <Contact />
+      <Footer />
     </main>
   );
 };
