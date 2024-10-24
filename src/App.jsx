41fe49ff6769cfb28c99935c7afe756aa56b2704
Ömer Faruk TARTAR <oftartar@gmail.com>
2024-10-24 16:08:46 +0300

    implement work experience section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./sections/About";
 import Clients from "./sections/Clients";
 import Contact from "./sections/Contact";
+import Experience from "./sections/Experience";
 import Footer from "./sections/Footer";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
@@ -14,7 +15,7 @@ const App = () => {
       <About />
       <Projects />
       <Clients />
-
+      <Experience />
       <Contact />
       <Footer />
     </main>
